dccddb3e2124fb1cd3f55d27edfa9cf9b0bab3db
Martin Lindberg <30564420+fartinmartin@users.noreply.github.com>
2024-01-07 15:45:35 -0800

    adjust tauri setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import "./styles.css";
-import App from "./App.svelte";
+import "./ui/styles/styles.css";
+import App from "./app.svelte";
 
 const app = new App({
-  target: document.getElementById("app"),
+	target: document.getElementById("app")!,
 });
 
 export default app;
